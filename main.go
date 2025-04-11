69787a1cec882769e8102486ed50d07c85f56e68
Cynthia Allen <cynthiaallen228@gmail.com>
2025-04-10 22:05:43 -0700

    begin process interaction

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"net"
+	"os"
+	"sync"
 
-	"gihub.com/scrmbld/course-site/procWeb"
+	"gihub.com/scrmbld/course-site/procrun"
 )
 
 // start the lua program
@@ -12,21 +14,44 @@ import (
 // scan the pipes
 // wait for messages from the sockets
 
+var wg = sync.WaitGroup{}
+
+func printOutput(out *bufio.Writer, outChan chan []byte, wg *sync.WaitGroup) {
+	defer wg.Done()
+	defer fmt.Println("done reading")
+	for msg := range outChan {
+		fmt.Print(string(msg))
+		out.Write(msg)
+	}
+}
+
 func main() {
-	listener, err := net.Listen("tcp", "0.0.0.0:4400")
-	if err != nil {
-		panic(err)
-	}
+	stdinChan := make(chan []byte, 8)
+	stdoutChan := make(chan []byte, 8)
+	stderrChan := make(chan []byte, 8)
+	wg.Add(1)
+	go procrun.RunLua("lua/test.lua", stdinChan, stdoutChan, stderrChan, &wg)
+	stdinScanner := bufio.NewScanner(os.Stdin)
+	stdoutWriter := bufio.NewWriter(os.Stdout)
+	stderrWriter := bufio.NewWriter(os.Stderr)
 
-	sock, err := listener.Accept()
-	if err != nil {
-		panic(err)
-	}
+	wg.Add(1)
+	go func() {
+		defer close(stdinChan)
+		defer wg.Done()
+		defer fmt.Println("done writing")
+		for stdinScanner.Scan() {
+			text := stdinScanner.Bytes()
+			text = append(text, []byte("\n")...)
+			fmt.Println(text)
+			stdinChan <- text
+		}
+	}()
 
-	msgChan := make(chan procWeb.ProcMessage, 5)
-	go procWeb.ScanProcConnection(sock, msgChan)
-	for msg := range msgChan {
-		fmt.Println(msg)
-		fmt.Println("======")
-	}
+	wg.Add(2)
+	go printOutput(stdoutWriter, stdoutChan, &wg)
+	go printOutput(stderrWriter, stderrChan, &wg)
+
+	wg.Wait()
+	fmt.Println("done")
 }
---
+++ procrun/procrun.go
@@ -0,0 +1,93 @@
+package procrun
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"io/fs"
+	"log"
+	"os/exec"
+	"sync"
+)
+
+func RunLua(program string, stdinChan chan []byte, stdoutChan chan []byte, stderrChan chan []byte, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	// prepare the process
+	proc := exec.Command("lua", program)
+
+	stdin, err := proc.StdinPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	stdout, err := proc.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	stderr, err := proc.StderrPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = proc.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// write to stdin
+	go func() {
+		for {
+			defer stdin.Close()
+			defer fmt.Println("closing stdin pipe")
+			var msg []byte
+			msg, ok := <-stdinChan
+			if ok == false {
+				break
+			}
+
+			_, err := stdin.Write(msg)
+			if err != nil {
+				if errors.Is(err, fs.ErrClosed) {
+					fmt.Println("stdin: closed")
+					break
+				}
+				log.Fatal("stdin: ", err)
+			}
+		}
+	}()
+
+	// read an output pipe
+	outScanner := func(pipe io.ReadCloser, outChan chan []byte, name string) {
+		defer pipe.Close()
+		defer close(outChan)
+
+		for {
+			msg := make([]byte, 2048)
+			n, err := pipe.Read(msg)
+			if err != nil {
+				if errors.Is(err, fs.ErrClosed) {
+					fmt.Println(name, ": closed")
+					return
+				}
+				if errors.Is(err, io.EOF) {
+					fmt.Println(name, ": EOF")
+					return
+				}
+				log.Fatal(name, ": ", err)
+			}
+
+			outChan <- msg[:n]
+		}
+	}
+
+	// read from stdout
+	go outScanner(stdout, stdoutChan, "stdout")
+	// read from stderr
+	go outScanner(stderr, stderrChan, "stderr")
+
+	err = proc.Wait()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("proc done")
+}
